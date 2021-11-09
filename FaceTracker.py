f210c00e6d7aa06fb74f48368249919973fe7cca
Sebastian Vargas <20197820@itla.edu.do>
2021-11-09 18:41:27 -0400

    Reconocimiento Facial

---
+++ FaceTracker.py
@@ -0,0 +1,29 @@
+import  cv2
+from simple_facerec import SimpleFacerec
+
+#Cargas las caras
+sfr=SimpleFacerec()
+sfr.load_encoding_images("images/")
+
+#Carga la camara
+cap=cv2.VideoCapture(0)
+
+
+
+while True:
+    ret,frame=cap.read()
+
+    #Detectar las caras(Localizacion de la cara)
+    face_locations,face_names=sfr.detect_known_faces(frame)
+    for face_loc,name in zip(face_locations,face_names):
+        top,left,bottom,right=face_loc[0],face_loc[1],face_loc[2],face_loc[3]
+        cv2.putText(frame,name,(left,top-10),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
+        cv2.rectangle(frame,(left,top),(right,bottom),(0,0,200),4)
+
+    cv2.imshow("Frame",frame)
+    key=cv2.waitKey(1)
+    if key==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
